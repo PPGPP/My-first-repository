cb3c7c67e1be55553e6794119ae115b68e28f632
PPGPP <piyaminipiyamini562@gmail.com>
2025-07-21 21:12:27 +0530

    Create a new program(ccode.c)
    I created a new program with put my general details.

---
+++ ccode.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  printf("1.My name is P.P.G.P.Piyamini.\n:);
+  printf("2.My age is 22.\n");
+  printf("3.My village is Homagama.\n");
+return 0;
+}
