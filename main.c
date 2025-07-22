1e996d3febaff6d868b87a572efe4c3ddc461a24
PPGPP <piyaminipiyamini562@gmail.com>
2025-07-22 09:39:20 +0530

    Update main.c
    I put questions for user

--- main.c
+++ main.c
@@ -5,6 +5,15 @@ int main()
 {
     printf("My name is .................\n");
     printf("My age is ...............\n");
+
+    char x[10];
+    int y;
+
+    printf("Enter your first name:");
+    scanf("%s",x);
+
+    printf("Enter your marks:");
+    scanf("%d",y);
     
     return 0;
 }
